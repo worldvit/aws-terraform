abebb280e8339d7fb40c70de2fc911f99690a1b5
worldvit <ccna.kim@gmail.com>
2024-06-05 15:47:02 +0900

    New:create-vpc-igw

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
+output "public_ip_1" {
   value = aws_instance.web[*].public_ip
+}
+
+output "public_ip_2" {
+  value = aws_instance.database[*].public_ip
 }
--- resources.tf
+++ resources.tf
@@ -1,8 +1,12 @@
-# Data section
+################
+# Data section #
+################
+# get a policy
 data "aws_iam_policy" "Administrator_Access" {
   name="AdministratorAccess"
 }
 
+# get an ami for amazon
 data "aws_ami" "amazon-linux" {
   most_recent = true
   owners = [ "amazon" ]
@@ -25,6 +29,7 @@ data "aws_ami" "amazon-linux" {
   }
 }
 
+# get an ami for ubuntu
 data "aws_ami" "ubuntu-linux" {
   most_recent = true
   owners = [ "099720109477" ]
@@ -47,6 +52,7 @@ data "aws_ami" "ubuntu-linux" {
   }
 }
 
+# get a subnet from default vpc
 data "aws_subnet" "subnet-0" {
   filter {
     name="tag:Name"
@@ -71,6 +77,7 @@ data "aws_subnet" "subnet-48" {
     values = ["default-subnet-48"]
   }
 }
+
 # Create users
 /*
 resource "aws_iam_user" "adam" {
@@ -85,7 +92,149 @@ resource "aws_iam_user" "eve" {
   }
 }
 */
+#####################
+# Resources Section #
+#####################
+#  create vpc
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.10.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+  tags = { Name = "vpc-1"}
+}
 
+# create public subnets
+resource "aws_subnet" "vpc1-public-subnet-0" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.0.0/20"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-west-2a"
+  tags = { Name = "public-subnet-0"}
+}
+
+resource "aws_subnet" "vpc1-public-subnet-16" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.16.0/20"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-west-2b"
+  tags = { Name = "public-subnet-16"}
+}
+
+resource "aws_subnet" "vpc1-public-subnet-32" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.32.0/20"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-west-2c"
+  tags = { Name = "public-subnet-32"}
+}
+
+resource "aws_subnet" "vpc1-public-subnet-48" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.48.0/20"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-west-2d"
+  tags = { Name = "public-subnet-48"}
+}
+
+# create private subnets
+resource "aws_subnet" "vpc1-private-subnet-64" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.64.0/20"
+  availability_zone = "us-west-2a"
+  tags = { Name = "private-subnet-64"}
+}
+
+resource "aws_subnet" "vpc1-private-subnet-80" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.80.0/20"
+  availability_zone = "us-west-2b"
+  tags = { Name = "private-subnet-80"}
+}
+
+resource "aws_subnet" "vpc1-private-subnet-96" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.96.0/20"
+  availability_zone = "us-west-2c"
+  tags = { Name = "private-subnet-96"}
+}
+
+resource "aws_subnet" "vpc1-private-subnet-112" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.10.112.0/20"
+  availability_zone = "us-west-2d"
+  tags = { Name = "private-subnet-112"}
+}
+
+# create Internet Gateway
+resource "aws_internet_gateway" "vpc1-igw" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = { Name = "vpc1-igw"}
+}
+
+# create a public route
+resource "aws_route_table" "vpc1-public" {
+  vpc_id = aws_vpc.vpc1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc1-igw.id
+  }
+  tags = { Name = "vpc1-public"}
+}
+
+# route associateions public
+resource "aws_route_table_association" "vpc1-public-subnet-0" {
+  subnet_id = aws_subnet.vpc1-public-subnet-0.id
+  route_table_id = aws_route_table.vpc1-public.id
+}
+
+resource "aws_route_table_association" "vpc1-public-subnet-16" {
+  subnet_id = aws_subnet.vpc1-public-subnet-16.id
+  route_table_id = aws_route_table.vpc1-public.id
+}
+
+resource "aws_route_table_association" "vpc1-public-subnet-32" {
+  subnet_id = aws_subnet.vpc1-public-subnet-32.id
+  route_table_id = aws_route_table.vpc1-public.id
+}
+
+resource "aws_route_table_association" "vpc1-public-subnet-48" {
+  subnet_id = aws_subnet.vpc1-public-subnet-48.id
+  route_table_id = aws_route_table.vpc1-public.id
+}
+
+# create a private route
+resource "aws_route_table" "vpc1-private" {
+  vpc_id = aws_vpc.vpc1.id
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.natgw.id
+  # }
+  tags = { Name = "vpc1-private"}
+}
+
+# route associateions private
+resource "aws_route_table_association" "vpc1-private-subnet-64" {
+  subnet_id = aws_subnet.vpc1-private-subnet-64.id
+  route_table_id = aws_route_table.vpc1-private.id
+}
+
+resource "aws_route_table_association" "vpc1-private-subnet-80" {
+  subnet_id = aws_subnet.vpc1-private-subnet-80.id
+  route_table_id = aws_route_table.vpc1-private.id
+}
+
+resource "aws_route_table_association" "vpc1-private-subnet-96" {
+  subnet_id = aws_subnet.vpc1-private-subnet-96.id
+  route_table_id = aws_route_table.vpc1-private.id
+}
+
+resource "aws_route_table_association" "vpc1-private-subnet-112" {
+  subnet_id = aws_subnet.vpc1-private-subnet-112.id
+  route_table_id = aws_route_table.vpc1-private.id
+}
+
+# create users
 resource "aws_iam_user" "managers" {
   count = length(var.users)
   name = var.users[count.index]
@@ -106,6 +255,7 @@ resource "aws_iam_group_policy_attachment" "attach" {
   policy_arn = data.aws_iam_policy.Administrator_Access.arn
 }
 
+# belongs to group
 resource "aws_iam_group_membership" "devops" {
     name = aws_iam_group.devops.name
     count = length(var.users)
@@ -120,9 +270,9 @@ resource "aws_s3_bucket" "bucket1" {
   bucket = "terraform-kdigital-shop-20240604-1"
 }
 
-# Create an instance
+# Create an instance using amazon ami
 resource "aws_instance" "web" {
-#   ami = "ami-06068bc7800ac1a83"
+  # ami = "ami-06068bc7800ac1a83"
   # ami = var.ami
   count = 3
   ami = data.aws_ami.amazon-linux.id
@@ -146,7 +296,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo amazon-linux-extras install nginx -y
+    sudo amazon-linux-extras install nginx1 -y
     sudo systemctl enable --now nginx
     echo "WEB Pages" | sudo tee /usr/share/nginx/html/index.html
     sudo systemctl restart nginx
@@ -157,6 +307,7 @@ resource "aws_instance" "web" {
   }
 }
 
+# Create an instance using ubuntu ami
 resource "aws_instance" "database" {
 #   ami = "ami-06068bc7800ac1a83"
   # ami = var.ami
@@ -169,6 +320,7 @@ resource "aws_instance" "database" {
     # aws_security_group.mysql.id
     ]
   subnet_id = data.aws_subnet.subnet-16.id
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size = "20"
@@ -201,6 +353,8 @@ resource "aws_instance" "web-2" {
   vpc_security_group_ids = [ aws_security_group.management.id ]
 }
 */
+
+# Create public key
 resource "aws_key_pair" "mykey" {
   key_name = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
