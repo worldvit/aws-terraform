797f7169bf6e7d0c10230e88153d08dd33dd09a2
worldvit <ccna.kim@gmail.com>
2024-06-05 14:46:39 +0900

    Mod:file:resources

--- resources.tf
+++ resources.tf
@@ -25,6 +25,28 @@ data "aws_ami" "amazon-linux" {
   }
 }
 
+data "aws_ami" "ubuntu-linux" {
+  most_recent = true
+  owners = [ "099720109477" ]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/*"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 data "aws_subnet" "subnet-0" {
   filter {
     name="tag:Name"
@@ -123,14 +145,50 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt udpate
-    sudo apt install nginx -y
+    sudo yum update -y
+    sudo amazon-linux-extras install nginx -y
     sudo systemctl enable --now nginx
-    echo "WEB Pages" | sudo tee /var/www/html/index.html
+    echo "WEB Pages" | sudo tee /usr/share/nginx/html/index.html
+    sudo systemctl restart nginx
   EOF
 
   tags = {
     Name = "web-${count.index}"
+  }
+}
+
+resource "aws_instance" "database" {
+#   ami = "ami-06068bc7800ac1a83"
+  # ami = var.ami
+  count = 3
+  ami = data.aws_ami.ubuntu-linux.id
+  instance_type = var.inst-type
+  key_name = aws_key_pair.mykey.key_name
+  vpc_security_group_ids = [ 
+    aws_security_group.management.id,
+    # aws_security_group.mysql.id
+    ]
+  subnet_id = data.aws_subnet.subnet-16.id
+
+  root_block_device {
+    volume_size = "20"
+    volume_type = "gp2"
+
+    tags = {
+      Name = "root-device"
+    }
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install nginx -y
+    sudo systemctl enable --now nginx
+    echo "WEB Pages" | sudo tee /usr/share/nginx/html/index.html
+  EOF
+
+  tags = {
+    Name = "database-${count.index}"
   }
 }
 /*
